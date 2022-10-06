470d6ddc3cd341811c6e3c78c8eec577452d9dea
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-06 14:21:10 +0500

    Create question3.js

---
+++ question3/question3.js
@@ -0,0 +1,12 @@
+let text = "ali";
+function titleCase(string) {
+    var sentence = string.toLowerCase().split(" ");
+    for(var i = 0; i< sentence.length; i++){
+       sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+    }
+document.write(sentence.join(" "));
+return sentence;
+}
+alert(text.toUpperCase());
+alert(text.toLowerCase());
+alert(titleCase(text));
