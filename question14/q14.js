d1621213ffd6e0e9439d670164bc0b0e89452998
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-06 20:42:22 +0500

    Create q14.js

---
+++ question14/q14.js
@@ -0,0 +1,10 @@
+let guest_list=[];
+for(i=0;i<3;i++)
+{
+    guest_list[i]=prompt("enter your guest:")
+}
+
+for(i=0;i<3;i++)
+{
+console.log("hello "+guest_list[i]+"how are you i wanted to invite you on my dinner party")
+}
