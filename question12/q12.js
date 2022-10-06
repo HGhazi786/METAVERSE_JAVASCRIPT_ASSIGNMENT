9549ab280e772a0d82a37836cd0d096d0948bdd1
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-06 15:24:41 +0500

    Update q12.js

--- question12/q12.js
+++ question12/q12.js
@@ -1,4 +1,4 @@
 const friends = ["husnain", "ali", "talal"];
-for (let i = 0; i < friends.length; i++) {
-    console.log('how are you '+friends[i]+' welcome to the class');
-  }
+console.log('how are you '+friends[1]+' welcome to the class');
+console.log('hello '+friends[2]+' how was the class');
+console.log('nice meeting you Mr. '+friends[3]);
