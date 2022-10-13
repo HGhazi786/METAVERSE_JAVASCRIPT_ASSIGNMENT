49771ef061b0ae02e67c12d04b999554427ad7e7
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-13 14:52:54 +0500

    Create q24.js

---
+++ question24/q24.js
@@ -0,0 +1,65 @@
+//answer is true
+var a = 5;
+console.log("Is a >= 3? I predict True.")
+console.log(a >= 3)
+//answer is false
+var a = 6;
+console.log("Is a <= 3? I predict True.")
+console.log(a <= 5)
+//answer is true
+var a = 5;
+console.log("Is a > 3? I predict True.")
+console.log(a > 3)
+//answer is false
+var a = 6;
+console.log("Is a < 3? I predict True.")
+console.log(a < 1)
+//answer is true
+var a = '1';
+console.log("Is a === 3? I predict True.")
+console.log(a === '1')
+//answer is false
+var a = '1';
+console.log("Is a === 3? I predict True.")
+console.log(a === 1)
+//answer is true
+var a = 1;
+console.log("Is a === 3? I predict True.")
+console.log(a === 1)
+//answer is false
+var a = 1;
+console.log("Is a != 3? I predict True.")
+console.log(a != 1)
+//answer is true
+var car = 'revo';
+console.log("Is car != 'civic'? I predict True.")
+console.log(car != 'civic')
+//answer is false
+var car = 'revo';
+console.log("Is car == 'civic'? I predict True.")
+console.log(car == 'civic')
+//answer is true
+var car = 'revo';
+console.log("Is car != 'civic'? I predict True.")
+console.log(car != 'civic')
+//answer is false
+var car = 'revo';
+console.log("Is car == 'REVO'? I predict True.")
+console.log(car == 'REVO')
+//answer is true
+var marks=92
+console.log("marks are greater then 90 but less than 95")
+console.log(marks>90&&marks<95)
+//answer is false
+var marks=62
+console.log("marks are greater then 90 but less than 95")
+console.log(marks>90&&marks<95)
+//item in an array
+//answer is true
+var arr=[1,2,3]
+console.log("does arr contain 2")
+console.log(arr.includes(2))
+//answer is false
+var arr=[1,2,3]
+console.log("does arr contain 4")
+console.log(arr.includes(4))
