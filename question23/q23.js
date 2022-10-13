b7e368e97ad76f81225041ffb21039fda3295e1b
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-13 14:32:08 +0500

    Create q23.js

---
+++ question23/q23.js
@@ -0,0 +1,40 @@
+//answer is true
+var a = 5;
+console.log("Is a >= 3? I predict True.")
+console.log(a >= 3)
+//answer is false
+var a = 6;
+console.log("Is a <= 3? I predict True.")
+console.log(a <= 5)
+//answer is true
+var a = 5;
+console.log("Is a > 3? I predict True.")
+console.log(a > 3)
+//answer is false
+var a = 6;
+console.log("Is a < 3? I predict True.")
+console.log(a < 1)
+//answer is true
+var a = '1';
+console.log("Is a === 3? I predict True.")
+console.log(a === '1')
+//answer is false
+var a = '1';
+console.log("Is a === 3? I predict True.")
+console.log(a === 1)
+//answer is true
+var a = 1;
+console.log("Is a === 3? I predict True.")
+console.log(a === 1)
+//answer is false
+var a = 1;
+console.log("Is a != 3? I predict True.")
+console.log(a != 1)
+//answer is true
+var car = 'revo';
+console.log("Is car != 'civic'? I predict True.")
+console.log(car != 'civic')
+//answer is false
+var car = 'revo';
+console.log("Is car == 'civic'? I predict True.")
+console.log(car == 'civic')
