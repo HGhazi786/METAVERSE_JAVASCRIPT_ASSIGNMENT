1ce69a6a24d75372c2789aa677a99f963629014f
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-13 15:48:14 +0500

    Create q30.js

---
+++ question30/q30.js
@@ -0,0 +1,10 @@
+let arr=[]
+for(let i=0;i<5;i++)
+{
+    arr[i]=prompt("enter user name")
+}
+for(let i=0;i<5;i++)
+{
+    if(arr[i]="admin"){console.log("hello "+arr[i]+" ready to take back control")}
+    console.log("hello "+arr[i]+" thankyou for logging in again")
+}
