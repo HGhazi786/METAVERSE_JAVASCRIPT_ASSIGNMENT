20c4b00db84f069c0847dcf8d4f2beb5aabac7eb
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-13 15:25:27 +0500

    Create q29.js

---
+++ question29/q29.js
@@ -0,0 +1,6 @@
+let fav_fruit=['apple','mango','banana'];
+for(let i in fav_fruit){if(fav_fruit[i]=='grapes'){console.log("you really like grapes!")}}
+for(let i in fav_fruit){if(fav_fruit[i]=='apple'){console.log("you really like apple!")}}
+for(let i in fav_fruit){if(fav_fruit[i]=='orange'){console.log("you really like orange!")}}
+for(let i in fav_fruit){if(fav_fruit[i]=='mango'){console.log("you really like mango!")}}
+for(let i in fav_fruit){if(fav_fruit[i]=='banana'){console.log("you really like banana!")}}
