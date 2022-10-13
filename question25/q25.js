0a791cc90b0fd67e8722ff1356120e89d9656292
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-13 15:07:12 +0500

    Create q25.js

---
+++ question25/q25.js
@@ -0,0 +1,6 @@
+let alien_color='green'
+if(alien_color==='green'){console.log("you have just earned 5 points")}
+else{console.log("failed if test")}
+alien_color='yellow'
+if(alien_color==='green'){console.log("you have just earned 5 points")}
+else{console.log("failed if test")}
