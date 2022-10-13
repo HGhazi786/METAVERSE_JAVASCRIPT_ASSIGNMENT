4ce7d3fab8826d6794fedbc8eee9d319095d3d32
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-13 15:05:23 +0500

    Create q26.js

---
+++ question26/q26.js
@@ -0,0 +1,6 @@
+let alien_color='green'
+if(alien_color==='green'){console.log("you have just earned 5 points")}
+else{console.log("you have just earned 10 points")}
+alien_color='yellow'
+if(alien_color==='green'){console.log("you have just earned 5 points")}
+else{console.log("you have just earned 10 points")}
