e1dbdbde4d57104434bafb00fa6bf39ddfec8173
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-15 07:49:27 +0500

    Create q31.js

---
+++ question31/q31.js
@@ -0,0 +1,8 @@
+let age=[]
+if(age.length===0){console.log("we need to find some users")}
+else if(age<2){console.log("person is baby")}
+else if(age>2&&age<4){console.log("person is toddler")}
+else if(age>4&&age<13){console.log("person is kid")}
+else if(age>13&&age<20){console.log("person is teenager")}
+else if(age>20&&age<65){console.log("person is adult")}
+else{console.log("person is elder")}
