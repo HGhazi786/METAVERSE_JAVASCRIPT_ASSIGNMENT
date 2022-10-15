549b90a8846c0401e72bb88f932f76eeea0fa645
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-15 21:04:55 +0500

    Create q33.js

---
+++ question33/q33.js
@@ -0,0 +1,43 @@
+//running in nodejs
+// import promptSync from 'prompt-sync'
+// const prompt=promptSync()
+let arr=[1,2,3,4,5,6,7,8,9]
+for(let i=0;i<arr.length;i++)
+{
+        if(arr[i]===1)
+        {
+            console.log(1+"st")
+        }
+        else if(arr[i]===2)
+        {
+            console.log(2+"nd")
+        }
+        else if(arr[i]===3)
+        {
+            console.log(3+"rd")
+        }
+        else if(arr[i]===4)
+        {
+            console.log(4+"th")
+        }
+        else if(arr[i]===5)
+        {
+            console.log(5+"th")
+        }
+        else if(arr[i]===6)
+        {
+            console.log(6+"th")
+        }
+        else if(arr[i]===7)
+        {
+            console.log(7+"th")
+        }
+        else if(arr[i]===8)
+        {
+            console.log(8+"th")
+        }
+        else{
+            console.log(9+"th")
+        }
+    
+}
