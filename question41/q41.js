224dbf7bbf221f5324e9589e2af3adbc751ccec7
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-17 12:08:06 +0500

    Create q41.js

---
+++ question41/q41.js
@@ -0,0 +1,3 @@
+function show_magician(arr=[]){for(let i=0;i<arr.length;i++){console.log("Magician: "+arr[i])}}//by default array will be empty
+let Magi_arr=["a","b","c","d","e"]
+show_magician(Magi_arr)
