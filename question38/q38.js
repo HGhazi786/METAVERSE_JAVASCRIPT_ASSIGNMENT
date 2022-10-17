85990dd6afcf34d22641a4a7eedcaee510fe7804
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-17 11:09:07 +0500

    Create q38.js

---
+++ question38/q38.js
@@ -0,0 +1,4 @@
+function describe_city(city,country='pakistan'){return city+' is in '+country}
+console.log(describe_city('rawalpindi'))
+console.log(describe_city('multan'))
+console.log(describe_city('karachi'))
