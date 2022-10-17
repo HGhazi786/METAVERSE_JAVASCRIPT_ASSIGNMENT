c5d3f7149f758b9d5e5351ed4c859c254a929d55
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-17 10:59:10 +0500

    Create q37.js

---
+++ question37/q37.js
@@ -0,0 +1,10 @@
+function make_shirt(size='large',msg='i love javascript')
+{
+    console.log("The size of the shirt is: "+size+" AND THE MESSAGE PRINTED ON it will be: "+msg)
+}
+let s1='medium'
+let s="small"
+let m="Web 3 is a revolution"
+make_shirt()
+make_shirt(s1)
+make_shirt(s,m)
