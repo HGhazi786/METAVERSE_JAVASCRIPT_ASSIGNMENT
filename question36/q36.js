2adcd2098335dd4ab2c48fe84871c07ec5d60a73
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-17 10:42:01 +0500

    Create q36.js

---
+++ question36/q36.js
@@ -0,0 +1,8 @@
+
+function make_shirt(size,msg)
+{
+    console.log("The size of the shirt is: "+size+" AND THE MESSAGE PRINTED ON it will be: "+msg)
+}
+let s="large"
+let m="Web 3 is a revolution"
+make_shirt(s,m)
