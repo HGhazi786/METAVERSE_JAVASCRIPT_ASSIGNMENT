d8ca9ccc093579c319c8742c9b1fd26310077c4f
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-17 10:41:23 +0500

    Create q35.js

---
+++ question35/q35.js
@@ -0,0 +1,6 @@
+let pet=['cow','sheep','goat']
+for(let i=0;i<3;i++)
+{
+    console.log("i love to keep a "+pet[i]+" as my pet");
+}
+console.log("They are used for farming and a great source of halaal meat");
