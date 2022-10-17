c2242fa77334d5e8a5af4d488b6a7317d0af9099
HGhazi786 <77294875+HGhazi786@users.noreply.github.com>
2022-10-17 11:16:18 +0500

    Create q39.js

---
+++ question39/q39.js
@@ -0,0 +1,4 @@
+function city_country(city,country){return '"'+city+','+country+'"'}
+console.log(city_country('rawalpindi','pakistan'))
+console.log(city_country('paris','france'))
+console.log(city_country('madrid','spain'))
